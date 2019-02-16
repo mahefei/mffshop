60f93a082b41d7ed57e6cd3051b0e9414c662ea7
mahefei <2329564320@qq.com>
2019-02-16 09:36:25 +0800

    users面包屑输入框按钮

--- src/components/home.vue
+++ src/components/home.vue
@@ -27,7 +27,7 @@
                 <span>用户管理</span>
               </template>
               <el-menu-item-group>
-                <el-menu-item index="1-1">
+                <el-menu-item index="users">
                   <i class="el-icon-menu"></i>
                   <span>用户列表</span>
                 </el-menu-item>
@@ -102,7 +102,11 @@
           </el-menu>
       
       </el-aside>
-      <el-main class="main">Main</el-main>
+      <el-main class="main">
+        <router-view>
+          
+        </router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
---
+++ src/components/users.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-card class="box">
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 搜索添加 -->
+    <el-row class="search">
+      <el-col>
+        <!-- 输入框 -->
+        <el-input placeholder="请输入内容" v-model="query" class="inputself">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+        <!-- 按钮 -->
+        <el-button type="success">搜索</el-button>
+      </el-col>
+    </el-row>
+    <!-- 表格 -->
+    <!-- 分页 -->
+  </el-card>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            query:''
+        }
+    }
+};
+</script>
+
+<style>
+.box {
+  height: 100%;
+}
+.search{
+    padding-top: 20px;
+}
+.inputself{
+    width: 400px;
+}
+</style>
